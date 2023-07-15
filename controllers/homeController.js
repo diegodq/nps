90f76f94f9ffe4a33d86e8d8d90406ce1c19ce29
Diego Sousa <pediegoss@gmail.com>
2023-07-14 23:39:16 -0300

    changes

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,6 +1,11 @@
 module.exports = {
 	async home(request, response)
 	{
-		return response.render('home/nps', { title: 'NPS - Automatiza Varejo' });
+		const cnpj = request.query.cnpj;
+		if(!cnpj) {
+			response.redirect(302, 'https://automatizavarejo.com.br');
+		} else {
+			return response.render('home/nps', { title: 'NPS - Automatiza Varejo' });
+		}
 	}
 }
